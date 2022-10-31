216fef566bcbef60bc8542dcae9c97015b8ddd3f
IndratejReddy <indratejreddy@gmail.com>
2022-10-31 17:07:55 +0530

    5th project

---
+++ DOM-Manipulation-Mini-Projects/5.js
@@ -0,0 +1,4 @@
+//[Samsung] ->(https://www.samsung.com/in/offer/online/samsung-fest/)
+
+let allButtons = document.querySelectorAll(".diwali-deals-product-sale-btn");
+allButtons.forEach((btn) => (btn.textContent = "Check out"));
