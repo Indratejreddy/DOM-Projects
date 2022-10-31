c1bd39b75a05a804fdd4aceac00007b9b64cb0f3
IndratejReddy <indratejreddy@gmail.com>
2022-10-31 17:08:34 +0530

    6th project

---
+++ DOM-Manipulation-Mini-Projects/6.js
@@ -0,0 +1,15 @@
+//[Adidas] ->(https://www.adidas.co.in/)
+
+//method 1:
+let search = document.querySelector(".searchinput___19uW0");
+search.addEventListener("mouseover", (e) => {
+  search.style.backgroundColor = "red";
+});
+
+//OR
+
+//method2:
+// let search = document.querySelector(".searchinput___19uW0");
+// search.addEventListener("mouseover", (e) => {
+//   e.target.style.backgroundColor = "red";
+// });
