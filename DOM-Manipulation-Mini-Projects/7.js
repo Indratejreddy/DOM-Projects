447b4db9dbc2cf4abab94d591eceb315ad465c9a
IndratejReddy <indratejreddy@gmail.com>
2022-11-02 15:17:19 +0530

    Project 7

---
+++ DOM-Manipulation-Mini-Projects/7.js
@@ -0,0 +1,11 @@
+//[MDN Web Docs] ->(https://developer.mozilla.org/en-US/)
+
+//method1
+document.getElementById("hp-search-input").value = "Javascript";
+document.getElementById("hp-search-form").submit();
+
+//or
+
+//method2:
+// document.querySelector("#hp-search-input").value = "Javascript";
+// document.getElementById("hp-search-form").submit();
