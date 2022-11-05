92523ce78a8b614092eb18ae1ce0623a1de47f6d
IndratejReddy <indratejreddy@gmail.com>
2022-11-05 10:12:58 +0530

    12th project

---
+++ DOM-Manipulation-Mini-Projects/12.js
@@ -0,0 +1,9 @@
+//[Github] ->(https://github.com/)
+
+document.querySelector(".btn.btn-sm.btn-primary.btn.mb-2").style.background = "red";
+
+//or 
+
+//method 2:
+
+// document.querySelector(".btn.btn-sm.btn-primary.btn.mb-2").setProperty("background-color", "red", "important");
