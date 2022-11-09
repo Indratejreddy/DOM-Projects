99d7b1e2b969342de950285abd432b7796b19334
IndratejReddy <indratejreddy@gmail.com>
2022-11-09 17:45:26 +0530

    Project 16

---
+++ DOM-Manipulation-Mini-Projects/16.js
@@ -0,0 +1,11 @@
+//[Vercel] ->(https://vercel.com/)
+
+document.querySelector(".section-title_title__VEDfK").innerHTML =
+    "Start with Scratch";
+  
+// or
+
+//2nd method:
+
+// document.querySelector(".section-title_title__VEDfK").firstChild.textContent =
+//   "Start with Scratch";
