455205e58771b9313181f8f0af52c8ea6b5798c8
IndratejReddy <indratejreddy@gmail.com>
2022-11-09 17:42:46 +0530

    Project 15

---
+++ DOM-Manipulation-Mini-Projects/15.js
@@ -0,0 +1,3 @@
+//[Dell] ->(https://www.dell.com/en-in/shop/deals/laptop-deals?gacd=10415953-9016-5761040-285981356-0&dgc=ST&gclid=Cj0KCQjwguGYBhDRARIsAHgRm4-XUDMhhVNyHXb3s1gY4ZBzORr_d9Se-buhJwy7asyUe7YdqEA11eEaAt6UEALw_wcB&gclsrc=aw.ds&nclid=BxjBlpBQsX6pjSHh-L8YYSU77EpfXRkG1AGMB5Wbeu386ykspfrPDnfx_DdFau20)
+
+document.querySelectorAll(".ps-title")[4].style.marginLeft = "105px";
